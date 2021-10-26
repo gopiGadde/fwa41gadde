9cd2b2b1bee900a98317074669b16c954c068cbf
gopiGadde <gopi05191998@gmail.com>
2021-10-25 21:27:09 -0500

    added variable

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var value = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  value++;
   res.send('respond with a resource');
 });
 
